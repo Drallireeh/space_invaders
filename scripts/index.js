9f52a567a5e11f9ca1d2a412727511fe960b4c44
HERILLARD Kenny <kenny.herillard@hotmail.fr>
2018-10-15 13:01:45 +0200

    Le joueur peut se déplacer

--- scripts/index.js
+++ scripts/index.js
@@ -1,14 +1,35 @@
+const canvas = document.getElementById('invaders');
+const context = canvas.getContext('2d');
+
+canvas.width = 480;
+canvas.height = 540;
+
+let timer;
+let player;
+
+// Chargement de l'image du sprite avant de démarrer le jeu
+const spritesheet = new Image();
+spritesheet.src = '../img/spritesheet.png';
+spritesheet.onload = function() { // fonction éxécutée lorsque le navigateur a finis de charger le PNG
+    player = createPlayer();
+
+    // Démarrage de la boucle continue
+    gameloop();
+};
 
 function update() {
-
+    animatePlayer();
 }
 
 function render() {
-
+    context.clearRect(0, 0, canvas.width, canvas.height);
+    renderPlayer();
 }
 
 // Fonction gérant la boucle de jeu
 function gameloop() {
     update();
     render();
+
+    timer = requestAnimationFrame(gameloop);
 }
